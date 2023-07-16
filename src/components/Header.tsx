e553591b2c38254e9140e0d210c10933147e8d29
Jibi George <jibiyyan@gmail.com>
2023-07-16 20:54:21 +0530

    Design

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+import React from 'react'
+
+const links = [
+    { href: '/', label: 'Home' }
+]
+
+const Header = () => {
+    return (
+        <header className='w-full h-fit px-10 py-3 sticky font-lato border-b border-[#967fa5]'>
+            <div className='w-full h-fit flex flex-row justify-between items-center'>
+                <div className='flex flex-row gap-4'>
+                    {links.map((link, linkIndex) => (
+                        <Link
+                            key={linkIndex}
+                            href={link.href}
+                            className='text-[#1e1d22] hover:text-[#AC6DDE] transition-colors font-lato font-bold text-lg'
+                        >
+                            {link.label}
+                        </Link>
+                    ))}
+                </div>
+                <div className='flex flex-row gap-4'>
+                    <span className='text-[#1e1d22] font-bold text-lg'>Language</span>
+                    <select name="" id="" className='min-w-[200px] bg-white rounded-[3px] px-2 text-[#1e1d22] border-black border'>
+                        <option>English</option>
+                        <option>Hindi</option>
+                    </select>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <h1>i18next</h1>
+    <div className="w-full px-10 py-10 font-lato text-center flex flex-col gap-2">
+      <p className="text-[#a39797] font-medium">HI THERE !</p>
+      <h1 className="text-4xl font-bold">I AM JIBI GEORGE</h1>
+      <p className="text-[#a39797] font-medium">MERN STACK DEVELOPER</p>
+    </div>
   )
 }
